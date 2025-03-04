a765c571e5508a05ac2d6168ccb7ad4aef302b30
4za4s <arbo3802@gmail.com>
2025-03-04 14:04:11 +1100

    astro config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://4za4s.github.io",
+    base: "MLP-Website"
+});
