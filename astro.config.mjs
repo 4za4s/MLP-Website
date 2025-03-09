5f535d3ac92abc175f38bea6f4d0137260ff63f7
4za4s <arbo3802@gmail.com>
2025-03-09 20:38:05 +1100

    Update website url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://4za4s.github.io",
-    base: "MLP-Website"
+    site: "https://mlpower.com.au",
+    base: ""
 });
